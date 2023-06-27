a5e8008279d708db99f6c73f141cc233e7799b2b
JunyaMasumura <137763774+JunyaMasumura@users.noreply.github.com>
2023-06-27 15:43:41 +0900

    Add files via upload

---
+++ Functions/FunctionFile.js
@@ -0,0 +1,16 @@
+﻿Office.initialize = function () {
+}
+
+// 情報バーにステータス メッセージを追加するヘルパー関数。
+function statusUpdate(icon, text) {
+  Office.context.mailbox.item.notificationMessages.replaceAsync("status", {
+    type: "informationalMessage",
+    icon: icon,
+    message: text,
+    persistent: false
+  });
+}
+
+function defaultStatus(event) {
+  statusUpdate("icon16" , "Hello World!");
+}
